ac065399fa917999d9bbbccd576902960acb9866
Hinen <qkrqldtjqj@naver.com>
2022-04-30 02:02:43 +0900

    Player Move Limit

--- Assets/Scripts/Scene_Game/Constants.cs
+++ Assets/Scripts/Scene_Game/Constants.cs
@@ -1,4 +1,7 @@
 public static class Constants {
+	public const int RESOLUTION_X = 2436;
+	public const int RESOLUTION_Y = 1125;
+	
 	public const int PLAYER_MAX_HP = 100;
 	public const float PLAYER_MOVE_SPEED = 500f;
 }
--- Assets/Scripts/Scene_Game/PlayerUnit.cs
+++ Assets/Scripts/Scene_Game/PlayerUnit.cs
@@ -1,7 +1,21 @@
 using UnityEngine;
 
 public class PlayerUnit : MonoBehaviour {
+    public Vector2 GamePosition => gameObject.transform.localPosition;
+    
     public void Move(Vector2 dir) {
-        gameObject.transform.Translate(Constants.PLAYER_MOVE_SPEED * dir * Time.deltaTime);
+        var moveTranslation = Constants.PLAYER_MOVE_SPEED * dir * Time.deltaTime;
+        var adjustValue = 150f;
+
+        if (dir == Vector2.left) {
+            if (GamePosition.x + moveTranslation.x <= -Constants.RESOLUTION_X / 2f + adjustValue)
+                return;
+        }
+        else if (dir == Vector2.right) {
+            if (GamePosition.x + moveTranslation.x >= Constants.RESOLUTION_X / 2f - adjustValue)
+                return;
+        }
+        
+        gameObject.transform.Translate(moveTranslation);
     }
 }
