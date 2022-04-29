2ab7cfdb756b9b8f720e177134e3e74dd4d46838
Hinen <qkrqldtjqj@naver.com>
2022-04-30 01:44:52 +0900

    PlayerHpBar

---
+++ Assets/Scripts/Scene_Game/PlayerHPBar.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHPBar : MonoBehaviour {
+    [SerializeField]
+    private Image _hpBarImage;
+    
+    private int _hp;
+    
+    public int Hp {
+        get {
+            return _hp;
+        }
+        set {
+            _hp = value;
+            
+            if (_hp > Constants.PLAYER_MAX_HP)
+                _hp = Constants.PLAYER_MAX_HP;
+
+            if (_hp < 0)
+                _hp = 0;
+            
+            _hpBarImage.fillAmount = (float)_hp / Constants.PLAYER_MAX_HP;
+            gameObject.SetActive(_hp != Constants.PLAYER_MAX_HP);
+        }
+    }
+    
+    public void Awake() {
+        Hp = 100;
+    }
+}
