8807a548c083f22213567fb9ac8c4dd0122593a4
Hinen <qkrqldtjqj@naver.com>
2022-04-30 01:14:15 +0900

    Create PlayerUnit

--- Assets/Scripts/Scene_Game/Scene_Game.cs
+++ Assets/Scripts/Scene_Game/Scene_Game.cs
@@ -1,5 +1,17 @@
 using UnityEngine;
 
 public class Scene_Game : MonoBehaviour {
+	[Header("World")]
+	[SerializeField]
+	private PlayerUnit _playerUnitPrefab;
+	private PlayerUnit _playerUnit;
+	public PlayerUnit PlayerUnit => _playerUnit;
 	
+	[SerializeField]
+	private GameObject _worldCanvas;
+	
+	public void Awake() {
+		_playerUnit = Instantiate(_playerUnitPrefab, _worldCanvas.transform);
+		_playerUnit.transform.localPosition = Vector3.zero;
+	}
 }
