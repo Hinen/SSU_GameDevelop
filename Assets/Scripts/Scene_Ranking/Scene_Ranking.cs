199652f6d9a1a856227d86c9f2825a40dbb4b1cb
Hinen <qkrqldtjqj@naver.com>
2022-05-20 01:25:29 +0900

    ranking insert test

--- Assets/Scripts/Scene_Ranking/Scene_Ranking.cs
+++ Assets/Scripts/Scene_Ranking/Scene_Ranking.cs
@@ -3,5 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Scene_Ranking : SceneBase {
-	
+	public void OnClickTest() {
+		RankingManager.Get().InsertRanking("hinen", 1234);
+	}
 }
