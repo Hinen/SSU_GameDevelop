2e9a01ee7761783997f697d605abcd52057dfda8
Hinen <qkrqldtjqj@naver.com>
2022-06-21 21:23:45 +0900

    Make Player

--- FINAL/Assets/Scripts/Scene_Game/Scene_Game.cs
+++ FINAL/Assets/Scripts/Scene_Game/Scene_Game.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
 
 public class Scene_Game : SceneBase {
+	[Header("World")]
 	[SerializeField]
 	private GameObject _worldCanvas;
 	public GameObject WorldCanvas => _worldCanvas;
+
+	[Header("Manager")]
+	[SerializeField]
+	private PoolManager _poolManager;
 	
 	public void Start() {
 		SoundManager.Get().PlayBGM(Constants.Sound.BGM.GAME);
