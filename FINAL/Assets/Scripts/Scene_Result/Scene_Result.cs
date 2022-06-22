ecf3e85b648273733a9756669bed36dbd1d07987
Hinen <qkrqldtjqj@naver.com>
2022-06-23 01:46:57 +0900

    Scene Result

---
+++ FINAL/Assets/Scripts/Scene_Result/Scene_Result.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Scene_Result : SceneBase {
+    [SerializeField]
+    private Text _scoreText;
+    
+    public void Start() {
+        SoundManager.Get().PlayBGM(Constants.Sound.BGM.TITLE);
+
+        _scoreText.text = GameManager.Get().Score.ToString();
+    }
+
+    public void OnClickScreen() {
+        SceneManager.LoadScene(Constants.SceneName.SCENE_TITLE);
+    }
+}
